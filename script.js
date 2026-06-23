20248868cabee86748ee0e32d5cb970d3c59b30a
Namrata jain <jnamrata477@gmail.com>
2026-06-23 19:58:30 +0530

    Added playing songs functionality by using basic JS function. Also added MP3 files

---
+++ script.js
@@ -0,0 +1,15 @@
+const audio = new Audio();//not sure what this is - blackbox
+
+document.querySelectorAll(".play-btn").forEach(function(button){
+
+    button.addEventListener("click", function(){
+
+        const songName = this.dataset.song;
+        audio.pause();             
+        audio.src = "./songs/" + songName;   
+        audio.play();               
+
+    });
+
+});
+//html me boht saare ho toh 
