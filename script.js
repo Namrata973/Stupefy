b6fb86f59c35dcdefd1b802f53181946eec4340d
Namrata jain <jnamrata477@gmail.com>
2026-06-24 09:40:37 +0530

    added comments in JS code for better understanding

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const audio = new Audio();//not sure what this is - blackbox
+// Create one audio player object.
+// Isse hum songs ko play, pause aur change kar sakte hain.
+const audio = new Audio();
 
-document.querySelectorAll(".play-btn").forEach(function(button){
+// Select all buttons jinki class "play-btn" hai.
+document.querySelectorAll(".play-btn").forEach(function(button) {
 
-    button.addEventListener("click", function(){
+    // Har button ke liye click event lagao.
+    button.addEventListener("click", function() {
 
+        // Jo button click hua hai uska data-song attribute read karo.
         const songName = this.dataset.song;
-        audio.pause();             
-        audio.src = "./songs/" + songName;   
-        audio.play();               
+
+        // Agar koi song pehle se chal raha hai to use stop (pause) karo.
+        audio.pause();
+
+        // Audio player ko batao kaunsa song load karna hai.
+        audio.src = "./songs/" + songName;
+
+        // Ab selected song play karo.
+        audio.play();
 
     });
 
